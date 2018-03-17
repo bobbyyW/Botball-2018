0c6c54e69f2d255c52060113d9be0fca21933de4
bobbyyW <bobbywang0@gmail.com>
2018-03-16 20:29:52 -0700

    Update main.c

--- Create/src/main.c
+++ Create/src/main.c
@@ -5,9 +5,6 @@
 
 #include <kipr/botball.h>
 #include "exmove.h"
-#include "initialDrive.h"
-#include "farmHayBales.h"
-#include "blueCow.h"
 
 //#define HAY_TEST // NOTE MAKE SURE THAT IF YOU ARE TESTING SOMETHING OTHER THAN HAY TO COMMENT THIS OUT
 
@@ -18,21 +15,15 @@ void init() {
 	printf("Conecting to Create\n");
 	// TODO Add light activate
 	create_connect();
-    printf("msleep\n");
-    //msleep(1500);
-    printf("start\n");
-    create_full();
+    	printf("start\n");
+    	create_full();
 	printf("connected\n");
 	// light here
-    set_servo_position(ARM_SERVO, ARM_UP);
-    set_servo_position(CLAW_SERVO, CLAW_CLOSED);    //set starting values
+    	set_servo_position(ARM_SERVO, ARM_UP);
+    	set_servo_position(CLAW_SERVO, CLAW_CLOSED);    //set starting values
 
-  enable_servo(ARM_SERVO);
+  	enable_servo(ARM_SERVO);
 	enable_servo(CLAW_SERVO);
-
-
-	initEx(POS_START_X, POS_START_Y, 0);
-    //arm(ARM_UP);
 
 }
 
@@ -43,7 +34,7 @@ void init() {
 void dinit() {
 	//disable_servo(ARM_SERVO);
 	//disable_servo(CLAW_SERVO);
-    disable_servos();   //Disable all servos - make sure you are not DQed
+    	disable_servos();   //Disable all servos - make sure you are not DQed
 	create_disconnect();
 	printf("Done!\n");
 }
@@ -53,19 +44,11 @@ int main() {
   	printf("init\n");
 	init();
   
-    wait_for_light(0);
-    shut_down_in(119);
-    create_start();
+    	wait_for_light(0);
+    	shut_down_in(119);
+    	create_start();
 
-#ifndef HAY_TEST
-	printf("Exec: initDrive\n");
-	initialDrive();
-	printf("Exec: blueCow\n");
-	blueCow();
-#endif
-	printf("Exec: hay\n");
-	farmHayBales();
-
+	//FUNCTIONS
 
 	printf("dinit\n");
 	dinit();
