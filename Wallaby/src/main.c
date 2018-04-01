9b3b13b72a3ce4ae126d65030a2f47c9eb067805
Austin K. Wang <austinkwang0@gmail.com>
2018-03-31 17:51:06 -0700

    slowServo, line square up, stop motor before servo. youre an extreme noob

--- Wallaby/src/main.c
+++ Wallaby/src/main.c
@@ -3,6 +3,21 @@ void servo(x,y) { //efficient servo positioning
     set_servo_position(x,y);
 }
 
+void slowServo(int servo, int goal, int milliseconds) {
+    int startPos = get_servo_position(servo);
+    int goalRelative = goal - startPos;
+    // how far to move the servo every time
+    float oneMove = (float)(goalRelative) / (milliseconds / 5.0); // 5 ms delay
+    //                      ^-- delta        ^-- number of loops
+
+    float pos;
+    for (pos = 0; fabsf(pos) < abs(goalRelative); pos += oneMove) {
+        set_servo_position(servo, (int)(pos + startPos));
+        msleep(5);
+    }
+    set_servo_position(servo, goal);
+}
+
 void move(x,y,z) { // efficient moving
 	motor(0,x); // 0 is right, 1 left
     motor(1,-y);
@@ -46,13 +61,16 @@ int main()
     int mean = (min + max)/2;
     int error = (max - min) * 0.05;
     int open = camera_open();
-    printf("%d\n",open);
+    printf("opened: %d\n",open);
     int update = camera_update();
-    printf("%d\n",update);
+    printf("updated: %d\n",update);
     int object_count = get_object_count(0);
-    printf("%d\n",object_count);
-    int object_area = get_object_area(0,1);
-    printf("%d\n",object_area);
+    printf("object count: %d\n",object_count);
+    int i;
+    for (i = 0; i < object_count; i = i + 1) {
+        int object_area = get_object_area(0,0);
+   		printf("object %d area: %d\n", i, object_area);
+    }
     
     //exit(0);
     
@@ -97,36 +115,68 @@ int main()
         	move(80,80,1); // noob
         }
     }
-    servo(2,340); // raise the claw before the noobish turn
+    
+    // stop motor before moving the claw
+    move(0,0,1);
+    slowServo(2,340, 1000); // raise the claw before the noobish turn
+    
     // Turn left
     while(analog(5) > mean - error) {
     	move(25,0,1);
     }
     
     // Turn left a bit more
-    move(25,0,750);
-    servo(2,1418);//move it down before the pickup
+    move(25,0,720);
+    // stop the motor
+    move(0,0,1);
+    slowServo(2,1350, 1000);//move it down before the pickup
     
     // Move forward to reach the yellow block
-    move(50,50,1500);
+    move(50,50,1000);
+    
+    // stop the motor
     // Close the claws
-    servo(0,250);
-    msleep(500);
+    move(0,0,1);
+    slowServo(0,250,1000);
+    //msleep(500);
     // Raise the claws
-    servo(2,1150);
+    move(0,0,1);
+    slowServo(2,1200,1000);
     
     // Move backward
-    move(-50,-50,2500);
-    
-    // Move right slightly so the right sensor is on white
-    move(0,25,1000);
-    
-    // Turn right until the right sensor in on black
-    while(analog(1) < mean) {
-    	move(0,25,1);
+    move(-50,-50,4250);
+        
+    // line square up
+    int left = analog(5);
+    int right = analog(1);
+    while (!(right < mean + error && right > mean - error && left < mean + error && left > mean - error)) {
+    	if (right < mean - error && left > mean + error) { // if right sensor is off but left is one, turn right
+        	move(-25,25,1);
+        }
+        else if (right > mean + error && left > mean + error) {
+        	move(25,25,1);
+        }
+        else if (right < mean - error && left < mean - error) {
+        	move (-25,-25,1);
+        }
+        else if (right > mean + error && left < mean - error) { // if right on black and left on white
+        	move(25,-25,1);
+        }
+        else {
+        	printf(" Noobish error. Right %d left %d\n", right, left);
+        }
+        right = analog(1);
+        left = analog(5);
     }
     
-
+    // Move right slightly so the right sensor is on white
+    //move(0,25,1000);
+    
+    /*// Turn right until the right sensor in on black
+    while(analog(1) < mean) {
+    	move(0,25,1);
+    }*/
+    
         
     return 0;
 }
