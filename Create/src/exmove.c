7bf132bb9b92323db5b3d4a9d64d8d360447fcae
bobbyyW <bobbywang0@gmail.com>
2018-04-01 20:19:50 -0700

    Update exmove.c

--- Create/src/exmove.c
+++ Create/src/exmove.c
@@ -23,3 +23,42 @@ void slowServo(int servo, int goal, int milliseconds) {
     }
     set_servo_position(servo, goal);
 }
+
+
+//HOWARD's ARM MOVEMENT STUFF
+
+void setup() {
+  clear_motor_position_counter(MOTOR_ARM);  //position at base, anchored at 0
+
+  clawOpen();
+
+}
+
+
+void botguyClose() {
+  set_servo_position(CLAW, BOTGUY_CLOSE);
+}
+
+void clawOpen() {
+  set_servo_position(CLAW, CLAW_OPEN);
+}
+
+
+void armUp(int distance, int power) {
+  distance = distance * 100;
+  
+  int initialPosition = get_motor_position_counter(MOTOR_ARM);
+  printf("Initial Position: %d", initialPosition);
+  power = -1 * power;
+  
+  motor(MOTOR_ARM, power);
+  int difference = abs(gmpc(MOTOR_ARM) - initialPosition);
+  while(difference < distance) {
+    printf("Position: %d\n", gmpc(MOTOR_ARM));
+    msleep(100);
+    difference = abs(gmpc(MOTOR_ARM) - initialPosition);
+  }
+  motor(MOTOR_ARM, 0);
+  
+}
+
