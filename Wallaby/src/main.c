a00681c2eb22abb1d595ef4a8afd10a49764913c
Austin K. Wang <austinkwang0@gmail.com>
2018-04-11 22:51:06 -0700

    Camera works, youre a noob

--- Wallaby/src/main.c
+++ Wallaby/src/main.c
@@ -24,40 +24,29 @@ void move(x,y,z) { // efficient moving
 	msleep(z);
 }
 
-/*void line_follow(x,y) {
-	int max = 3700;
-    int min = 200;
-    int mean = (min + max)/2;
-    int error = (max - min) * 0.05;
-    while (1) {
-    	int x = analog(5);
-        printf("%d\n",x);
-        if (x > mean + error) { // if it's more on the black
-        	move(x,y,1);
-        }
-        else if (x < mean - error) { // if it's more on the white
-        	move(y,x,1);
-        }
-        else {
-        	move(y,y,1); // noob
-        }
-    }*/
-
-
-//void turn_right() { //turn 90 degrees right
-	//move(-50,100,680);
-//}
-void turn_left(x) { //turn 90 degrees left
-	move(100,-50,540 - x);
+int detect_crate(area_thresh,confidence_thresh) {
+	camera_open();
+    camera_update();
+    get_object_count(0);
+    int object_area = get_object_area(0,0);
+    double object_confidence = get_object_confidence(0,0);
+    if (object_area > area_thresh && object_confidence > confidence_thresh) {
+    	return 1;
+    }
+    else {
+    	return 0;
+    }
 }
+
 
 int main()
 {   
+    
     enable_servos();
     servo(0,909);
     servo(2,1400);
-    int max = 3700;
-    int min = 200;
+    int max = 3400;
+    int min = 180;
     int mean = (min + max)/2;
     int error = (max - min) * 0.05;
     int open = camera_open();
@@ -68,35 +57,40 @@ int main()
     printf("object count: %d\n",object_count);
     int i;
     for (i = 0; i < object_count; i = i + 1) {
-        int object_area = get_object_area(0,0);
-   		printf("object %d area: %d\n", i, object_area);
+        int object_area = get_object_area(0,i);
+        //point2 object_center = get_object_center(0,i);
+        double object_confidence = get_object_confidence(0,i);
+   		printf("object %d confidence: %lf area: %d\n", i, object_confidence, object_area);
     }
     
     //exit(0);
-    
+    /*
     // Enter the first black line
     while(analog(1) < mean) {
     	move(50,50,1);
+        printf("enter(1) : %d\n",analog(1));
     }
 	
     // Exit the first black line
     while (analog(1) >= mean) {
-    	move(25,25,1);
+    	move(10,10,1);
+        printf("exit(1) : %d\n",analog(1));
     }
 	
     // Enter the second black line
     while (analog(1) < mean) {
-    	printf("%d\n",analog(5));
+    	printf("enter(2) : %d\n",analog(5));
         move(30,30,1);
     }
     
     // Exit the second black line
-    while (analog(1) >= mean - error) {
+    while (analog(1) >= mean) {
     	move(25,25,1);
+        printf("exit(2) : %d\n",analog(1));
     }
    
     //turn_right();
-    
+    */
     // Turn right until left sensor is on black
     while(analog(1) < mean) {
     	move(0,25,1); // regular -25, 25
@@ -124,9 +118,9 @@ int main()
     while(analog(5) > mean - error) {
     	move(25,0,1);
     }
-    
-    // Turn left a bit more
-    move(25,0,720);
+
+    // Go forward a bit more
+    move(25,25,720);//originally 25,0,720
     // stop the motor
     move(0,0,1);
     slowServo(2,1350, 1000);//move it down before the pickup
@@ -143,40 +137,114 @@ int main()
     move(0,0,1);
     slowServo(2,1200,1000);
     
-    // Move backward
-    move(-50,-50,4250);
+    //move backwards, enter black line
+    //while (analog(5) < mean + error) {
+    //	move(-50,-50,1);
+    //}
+    
+    //move back, exit black line
+    //while(analog(5) > mean + error) {
+    //	move(-25,-25,1);
+    //}
+    
+    
+    //Move backward
+    move(-50,-50,7100); // regular 4250
         
     // line square up
-    int left = analog(5);
-    int right = analog(1);
-    while (!(right < mean + error && right > mean - error && left < mean + error && left > mean - error)) {
-    	if (right < mean - error && left > mean + error) { // if right sensor is off but left is one, turn right
-        	move(-25,25,1);
-        }
-        else if (right > mean + error && left > mean + error) {
-        	move(25,25,1);
-        }
-        else if (right < mean - error && left < mean - error) {
-        	move (-25,-25,1);
-        }
-        else if (right > mean + error && left < mean - error) { // if right on black and left on white
-        	move(25,-25,1);
-        }
-        else {
-        	printf(" Noobish error. Right %d left %d\n", right, left);
-        }
-        right = analog(1);
-        left = analog(5);
-    }
+    //int left = analog(5);
+    //int right = analog(1);
+    //while (!(right < mean + error && right > mean - error && left < mean + error && left > mean - error)) {
+    //	if (right < mean - error && left > mean + error) { // if right sensor is off but left is one, turn right
+    //    	move(-25,25,1);
+    //    }
+    //    else if (right > mean + error && left > mean + error) {
+    //    	move(25,25,1);
+    //    }
+    //    else if (right < mean - error && left < mean - error) {
+    //    	move (-25,-25,1);
+    //    }
+    //    else if (right > mean + error && left < mean - error) { // if right on black and left on white
+    //    	move(25,-25,1);
+    //    }
+    //    else {
+    //    	printf(" Noobish error. Right %d left %d\n", right, left);
+    //    }
+    //    right = analog(1);
+    //    left = analog(5);
+    //}
     
     // Move right slightly so the right sensor is on white
     //move(0,25,1000);
     
-    /*// Turn right until the right sensor in on black
+    // Turn right until the right sensor in on black
     while(analog(1) < mean) {
     	move(0,25,1);
-    }*/
+    }
     
-        
+    // make a right turn
+    
+    while (analog(1) < mean) {
+    	move(-25,25,1);
+    }
+    
+    // line follow forward 'till black line
+    
+    while (analog(5) < mean) {
+    	int x = analog(1);
+        if (x > mean + error) {
+        	move(80,60,1);
+            printf("%d/",x);
+        }
+        else if (x < mean - error) {
+        	move(60,80,1);
+            printf("%d/",x);
+        }
+        else {
+        	move(80,80,1);
+            printf("%d/",x);
+        }
+    }
+    
+    while (analog(5) > mean) {
+    	move(25,-25,1);
+    }
+    while (analog(5) < mean) {
+    	move(25,-25,1);
+    }
+    
+    //line follow forwards
+    while(detect_crate(4000,0.35) == 0) {
+    	int x = analog(5);
+        if (x > mean + error) {
+        	move(60,80,1);
+        }
+        else if (x < mean - error) {
+        	move(80,60,1);
+        }
+        else {
+        	move(80,80,1);
+        }
+    }
+    //move(-50,-50,500);
+    
+    //line follow backward
+    
+    /*while (analog(5) < mean - error) { //change later
+    	int x = analog(1);
+        printf("%d\n",x);
+        if (x > mean + error) {// if it's on black, move left backwards
+        	move(-40,-20,1);
+        }
+        else if (x < mean - error) { // if it's on white, move right backwards
+        	move(-35,-40,1);
+            //printf("%d/",x);
+        }
+        else { // if it's on the edge, move backwards straight
+        	move(-40,-40,1);
+            //printf("%d/",x);
+        }
+    }  */   
+    
     return 0;
 }
